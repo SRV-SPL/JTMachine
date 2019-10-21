c5fa58bdcee19e8e8b592cc1113bac9e59345071
Arvin1011 <37037706+Arvin1011@users.noreply.github.com>
2019-10-21 23:40:14 +0800

    上传文件
    第一次使用github上传文件

---
+++ BinaryHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace Common
+{
+    public class BinaryHelper
+    {
+        public static object DeepClone(object objsrc)
+        {
+            object obj = null;
+            //将对象序列化成内存中的二进制流  
+            BinaryFormatter inputFormatter = new BinaryFormatter();
+            MemoryStream inputStream;
+            using (inputStream = new MemoryStream())
+            {
+                inputFormatter.Serialize(inputStream, objsrc);
+            }
+            //将二进制流反序列化为对象  
+            using (MemoryStream outputStream = new MemoryStream(inputStream.ToArray()))
+            {
+                BinaryFormatter outputFormatter = new BinaryFormatter();
+                obj = outputFormatter.Deserialize(outputStream);
+            }
+            return obj;
+        }
+
+        public static bool SerializeObj(object objsrc,string filename)
+        {
+            //将对象序列化成内存中的二进制流  
+            BinaryFormatter inputFormatter = new BinaryFormatter();
+            using (FileStream inputStream = new FileStream(filename,FileMode.OpenOrCreate))
+            {
+                inputFormatter.Serialize(inputStream, objsrc);
+            }
+            return true;
+        }
+
+        public static object DeserializeObj(string filename)
+        {
+            object obj = null;
+            if (!File.Exists(filename)) return null;
+            //将二进制流反序列化为对象  
+            using (FileStream outputStream = new FileStream(filename,FileMode.Open))
+            {
+                BinaryFormatter outputFormatter = new BinaryFormatter();
+                obj = outputFormatter.Deserialize(outputStream);
+            }
+            return obj;
+        }
+
+    }
+}
